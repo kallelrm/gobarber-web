151980b40ac5b5b2f731d37205792ec1fba77750
Kallel Roman <kallelr@outlook.com>
2020-12-02 16:08:36 -0300

    Habilitando CORS

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
     <GlobalStyle />
+    <AuthContext.Provider value={{ name: 'Kallel' }}>
+      <SignIn />
+    </AuthContext.Provider>
   </>
 );
 
